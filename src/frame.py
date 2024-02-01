3d2c043c386a027447d5297d68ccf2ae961c43b1
Evan Silva González <a20evansg@ateis.local>
2024-02-01 10:58:32 +0100

    test(test_frame.py):adding test folder and file

--- src/frame.py
+++ src/frame.py
@@ -6,3 +6,4 @@ def Frame():
         self.points =  ""
 
     
+    
---
+++ test/test_frame.py
@@ -0,0 +1,8 @@
+import pytest
+from src.frame import 
+
+
+@pytest.fixture
+def inyector():
+    tabla = tablaAsignacion()
+    return tabla
