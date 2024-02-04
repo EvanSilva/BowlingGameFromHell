f173df9b4b1f66a18766a7803d2259b269498254
AmarinoLama <amarinolama@gmail.com>
2024-02-04 20:24:36 +0100

    feat(ScoreCard): creating asignZero and asignFormat

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -8,7 +8,13 @@ class ScoreCard:
 
     def getTotalScore(self):
         total = 0
-        for pin in self.getPins():
+        for pin in self.asignFormat():
             total += int(pin)
         return total
        # return sum(int(pin) for pin in self.getPins())
+    
+    def asignZero(self):
+        return self.getPins().replace("-","0")
+    
+    def asignFormat(self):
+        return self.asignZero()
--- test/test_frame.py
+++ test/test_frame.py
@@ -1,9 +1,11 @@
 import pytest
 from src.scoreCard import ScoreCard
 
-
 def test_getPins():
     assert ScoreCard('12345123451234512345').getPins() == '12345123451234512345'
 
 def test_getScoreAvarage():
     assert ScoreCard('12345123451234512345').getTotalScore() == 60
+    
+def test_getScoreWithNull():
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getTotalScore() == 90
