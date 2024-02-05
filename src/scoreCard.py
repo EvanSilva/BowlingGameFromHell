dd4dbac441139670373acd6a4ffcef1ff9f27a3c
AmarinoLama <amarinolama@gmail.com>
2024-02-05 10:31:34 +0100

    feat(scoreCard): creating getFrames and getFramesByPosition

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -2,13 +2,15 @@ class ScoreCard:
 
     def __init__(self,pins):
         self.pins = pins
+        self.lenPins = len(self.getPins())
+        self.frames = self.getFrames()
     
     def getPins(self):
         return self.pins
 
     def getTotalScore(self):
         total = 0
-        for pin in self.asignFormat():
+        for pin in self.getPins():
             total += int(pin)
         return total
        # return sum(int(pin) for pin in self.getPins())
@@ -16,5 +18,22 @@ class ScoreCard:
     def asignZero(self):
         return self.getPins().replace("-","0")
     
-    def asignFormat(self):
-        return self.asignZero()
+    def getFrames(self):
+        frames = []
+        listedPins = list(self.getPins())
+        position = 0
+        
+        while position < len(listedPins):
+            if listedPins[position] != "x":
+                frames.append(listedPins[position:position+2])
+                position += 2
+            else:
+                frames.append([listedPins[position]])
+                position += 1
+        return frames
+
+    def getFrameByPosition(self,position):
+        if self.getPins()[position] == "x":
+            return "x"
+        else:
+            return self.getPins()[position:position+2]
--- test/test_frame.py
+++ test/test_frame.py
@@ -7,5 +7,13 @@ def test_getPins():
 def test_getScoreAvarage():
     assert ScoreCard('12345123451234512345').getTotalScore() == 60
     
+'''    
 def test_getScoreWithNull():
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getTotalScore() == 90
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getTotalScore() == 90 '''
+    
+def test_getFrames():
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getFrames() == [["9","-"],["9","-"],["9","-"],["9","-"],["9","-"],["9","-"],["9","-"],["9","-"],["9","-"],["9","-"] ]
+    
+def test_getFrameByPosition():
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getFrameByPosition(2) == "9-"
+    assert ScoreCard('9-x9-').getFrameByPosition(2) == "x" 
