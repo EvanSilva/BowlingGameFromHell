6c13b602b93ff414175270527d66a9a524be1e29
AmarinoLama <amarinolama@gmail.com>
2024-02-05 23:32:35 +0100

    feat(ScoreCard): creating a handful of functions

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -11,22 +11,31 @@ class ScoreCard:
     def getPins(self):
         return self.pins
 
+    #* tuve que cambiar el getPins por asignZero que daba error en un caso test
+
     def getTotalScore(self):
         total = 0
-        for pin in self.getPins():
+        for pin in self.asignZero():
             total += int(pin)
         return total
        # return sum(int(pin) for pin in self.getPins())
     
+    #? igual se debería cambiar el nombre por pinsWithZero o algo así
+
     def asignZero(self):
         return self.getPins().replace("-","0")
     
+    #* meto el asignZero en get frames para poder trabajar mejor a la hora de sumar
+
+    #* cambié la lógica de la función getFrames LEER ABAJO DE TODO
+
     def getFrames(self):
         frames = []
-        pins = self.getPins()
+        pins = self.asignZero()
         
         position = 0
-        while position < len(pins) and len(frames) < 9:
+        while position < len(pins):
+        #while position < len(pins) and len(frames) < 9:
             currentPin = pins[position]
 
             if currentPin != "x":
@@ -37,14 +46,90 @@ class ScoreCard:
                 frames.append(currentPin)
                 position += 1
 
-        if len(frames) == 9:
-            frames.append(pins[position:])
+        #if len(frames) == 9:
+            #frames.append(pins[position:])
                 
         return frames
 
+    #def getFrameByPosition(self,position):
+        #if self.getPins()[position] == "x":
+            #return "x"
+        #else:
+            #return self.getPins()[position:position+2]
+
+    #* al tener la función que te extraiga todos los frames la lógica de esta función se estropea por lo que quedaría de la siguiente forma, mucho más simplificada
+
     def getFrameByPosition(self,position):
-        if self.getPins()[position] == "x":
-            return "x"
+        return self.getFrames()[position]
+    
+    #* en esta función calculo un único frame mediante la posición del frame, se usa la función de getFramesByPosition y se junta el valor uno del frame y el valor dos
+    #* puse el nombre de sumFrame porque si era calculateFrame podía llegar a dar confusión a pensar que también calculaba Strikes, Semiplenos, ...
+
+    def sumFrame(self,position):
+        return int(self.getFrameByPosition(position)[0]) + int(self.getFrameByPosition(position)[1])
+
+    #* funcion con este nombre para diferenciarlo de la futura funcion de "calculateFinalStrike". Esta funcion calcula un strike mediante la posicion de este mismo para poder acceder al siguiente frame de una forma más sencilla
+    #? hay un problema con esta funcion cuando está en el penúltimo frame ya que uso la funcion sumFrame, sugiero cambiar la estructura del último frame y pasar de algo como "x12" a "x", "12", es decir, cambiarlo y dejar al final un único frame (otro ejemplo "1/3" a "1/", "3" y otro ejemplo "xxx" a "x", "x", "x") LEER ABAJO DE TODO
+    #? este cambio arreglaría las funciones calculateMidleStrike y calculateMidleSplit cuando se encuentren en el penúltimo frame LEER ABAJO DE TODO
+
+    def calculateMidleStrike(self,position):
+        firstNextFrame = self.getFrameByPosition(position+1)
+        secondNextFrame = self.getFrameByPosition(position+2)
+
+        #TODO caso1 en el que el siguiente frame no es un pleno
+        if firstNextFrame != "x":
+            return self.sumFrame(position+1) + 10
+        #TODO caso2 en el que los dos siguientes frames son plenos
+        elif firstNextFrame == "x" and secondNextFrame == "x":
+            return 30
+        #TODO caso3 en el que los siguientes frames son un pleno y una tirada normal
         else:
-            return self.getPins()[position:position+2]
+            return int(secondNextFrame[0]) + 20
+
+    #* funcion con prácticamente la misma lógica que calculateMidleStrike pero reducido a sumarle solo el siguiente tiro
+
+    def calculateMidleSplit(self,position):
+        nextFrame = self.getFrameByPosition(position+1)
+
+        #TODO caso1 en el que el siguiente frame no sea un pleno
+        if nextFrame != "x":
+            return int(nextFrame[0]) + 10
+        #TODO caso 2 en el que siguiente frame sea un pleno
+        else:
+            return 20
         
+    #* funcion que calcula el último frame cambiando los plenos y semiplenos por dieces y luego sumando todo
+    #? no entiendo porque el código de esta función está toda en azul a ver si le podeis echar un vistazo y arreglarla
+
+    def calculateLastFrame(self):
+        lastFrame = self.getFrames()[9:]
+
+        for i in range(len(lastFrame)):
+            if '/' in lastFrame[i] or lastFrame[i] == "x":
+                lastFrame[i] = 10
+            elif len(lastFrame[i]) > 1:
+                lastFrame[i] = self.sumFrame(9+i)
+        return sum(int(x) for x in lastFrame)
+    
+    def calculatePuntuation(self):
+        total = 0
+
+        for position, frame in enumerate(self.getFrames()[:9]):
+            if frame != "x" and "/" not in frame:
+                total += self.sumFrame(position)
+            elif frame == "x":
+                total += self.calculateMidleStrike(position)
+            elif "/" in frame:
+                total += self.calculateMidleSplit(position)
+
+        return total + self.calculateLastFrame()
+
+
+
+#? hay algo que no comprendo de la lógica de la puntuación, en el caso de que en el frame 9 tengas un pleno se sigue sumando los 2 siguientes no? (si es así se debe hacer lo de la línea 69). Y otra pregunta sobre el último frame, en el caso de que sean 3 plenos son 30 puntos o 90, son 30 no?
+        
+#* LEER AQUÍ, al final como vi que iba bien de tiempo y no tenía nada que hacer decidí ya cambiar la función getFrames para que el último frame fuese igual que el resto para pasar los casos test de calculateMidle... . Nosotros sabemos que a partir del noveno frame en adelante está todo en el mismo frame, esto lo podemos implementar en la funcionTotalScore tipo: "del frame 0 al 8 calcula de esta forma y del frame 9 en adelante de esta otra", espero haberme explicado, si teneis alguna duda mandarme un mensaje que igual ando con el móvil en la depilación
+    
+#* sorprendentemente el programa funciona hasta con un caso test de los dificiles ('x9-1/54--xx9-9/xx-'), como sugerencia final propongo eliminar la funcion de getTotalScore porque después de la de calculatePuntuation no sirve para nada
+    
+#TODO bueno chavales aquí está el código hecho por Amán ahora os toca refinarlo y hacerlo más bonito y legible mucha suerte chicos y lo siento por no estar presente para ayudaros. Un saludo, Amán
--- test/test_frame.py
+++ test/test_frame.py
@@ -7,16 +7,73 @@ def test_getPins():
 def test_getScoreAvarage():
     assert ScoreCard('12345123451234512345').getTotalScore() == 60
     
-'''   
+  
 def test_getScoreWithNull():
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getTotalScore() == 90  ''' 
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getTotalScore() == 90
     
+#* como implementé la función asignZero en la de getFrames tuve que cambiar todos los "-" por "0"
+#* modifiqué los casos test porque modifiqué esta función para poder pasar los casos test de calculateMidle...
+#* he modificado los casos test para que se completen todas las posibles combinaciones del último frame
+
 def test_getFrames():
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-x').getFrames() == ['9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', 'x']
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-xxx').getFrames() == ['9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', 'xxx']
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-x1/').getFrames() == ['9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', '9-', 'x1/']
-    assert ScoreCard('x9-1/56--xx9-9/xx-').getFrames() == ['x', '9-', '1/', '56', '--', 'x', 'x', '9-', '9/', 'xx-']
-    
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', '90']
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-xxx').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', 'x', 'x', 'x']
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x1/').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', 'x', '1/']
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x11').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', 'x', '11']
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/1').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', '1/', '1']
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/x').getFrames() == ['90', '90', '90', '90', '90', '90', '90', '90', '90', '1/', 'x']
+    assert ScoreCard('x9-1/56--xx9-9/xx-').getFrames() == ['x', '90', '1/', '56', '00', 'x', 'x', '90', '9/', 'x', 'x', '0']
+
+#* como implementé la función asignZero en la de getFrames tuve que cambiar todos los "-" por "0"
+
 def test_getFrameByPosition():
-    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getFrameByPosition(2) == "9-"
-    assert ScoreCard('9-x9-').getFrameByPosition(2) == "x" 
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').getFrameByPosition(2) == "90"
+    #* test modificado que ponía un 2 en vez de un 1
+    assert ScoreCard('9-x9-').getFrameByPosition(1) == "x" 
+
+#* casos test para la función sumFrame
+    
+def test_sumFrame():
+    assert ScoreCard("12-41234").sumFrame(0) == 3
+    assert ScoreCard("12-41234").sumFrame(1) == 4
+
+#* casos test para la funcion calculateMidleStrike 
+    
+def test_calculateMidleStrike():
+    assert ScoreCard('xxx').calculateMidleStrike(0) == 30
+    assert ScoreCard('x9-1/56--xx9-9/xx-').calculateMidleStrike(0) == 19
+    assert ScoreCard('x9-1/56--xx9-9/xx-').calculateMidleStrike(5) == 29
+    #? arreglar el código para este caso test (posible solución indicada en el otro archivo)
+    #? no sé si este último caso test está bien hecho porque me lié a última hora con la lógica del programa
+    assert ScoreCard('9-9-9-9-9-9-9-9-xxx').calculateMidleStrike(8) == 30
+
+#* casos test para la funcion calculateMidleSplit
+    
+def test_calculateMidleSplit():
+    assert ScoreCard('x9-1/x--xx9-9/xx-').calculateMidleSplit(2) == 20
+    assert ScoreCard('x9-1/56--xx9-9/xx-').calculateMidleSplit(2) == 15
+    #? pasa lo mismo que arriba con este caso test
+    assert ScoreCard('x9-1/56--xx9-9/xx-').calculateMidleSplit(8) == 20
+
+#* casos test para la funcion calculateLastFrame
+
+def test_calculateLastFrame():
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').calculateLastFrame() == 9
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-xxx').calculateLastFrame() == 30
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x1/').calculateLastFrame() == 20
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x11').calculateLastFrame() == 12
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/1').calculateLastFrame() == 11
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/x').calculateLastFrame() == 20
+    assert ScoreCard('x9-1/56--xx9-9/xx-').calculateLastFrame() == 20
+
+#* casos test para la funcion calculatePuntuation
+    
+def test_calculatePuntuation():
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-9-').calculatePuntuation() == 90
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-xxx').calculatePuntuation() == 111
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x1/').calculatePuntuation() == 101
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-x11').calculatePuntuation() == 93
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/1').calculatePuntuation() == 92
+    assert ScoreCard('9-9-9-9-9-9-9-9-9-1/x').calculatePuntuation() == 101
+    #* cambié el 56 por un 54 porque supuestamente no pueden haber más de 10 bolos, creo yo vamos
+    assert ScoreCard('x9-1/54--xx9-9/xx-').calculatePuntuation() == 149
